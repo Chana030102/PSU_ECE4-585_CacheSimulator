676f1c9ebf6bf4da5482234e1d3aa1f02b45bd59
Aaron Chan <chanaar@pdx.edu>
2018-03-05 16:37:55 -0800

    Format and add Makefile

--- Cache_Files/main.c
+++ Cache_Files/main.c
@@ -3,40 +3,40 @@
 
 int main()
 {
-/* get from user */
-cache_ways = 8;
-byte_select = 7;
-cache_index = 10;
+    /* get from user */
+    cache_ways = 8;
+    byte_select = 7;
+    cache_index = 10;
 
-int cache_tag = 32 - (cache_index + byte_select);
-double cache_sets_max = pow(2,cache_index);
-printf("Sets: %d\n",(int)cache_sets_max);
+    int cache_tag = 32 - (cache_index + byte_select);
+    double cache_sets_max = pow(2,cache_index);
+    printf("Sets: %d\n",(int)cache_sets_max);
 
-/* variables */
-int bs_bits[byte_select];
-char hex_address[HEX];
-int binary_address[BIN];
-struct cache_set s_cache[(int)cache_sets_max];
+    /* variables */
+    int bs_bits[byte_select];
+    char hex_address[HEX];
+    int binary_address[BIN];
+    struct cache_set s_cache[(int)cache_sets_max];
 
-strcpy(hex_address,"A0056FB9");
+    strcpy(hex_address,"A0056FB9");
 
-hex_to_bin(hex_address,binary_address,HEX);
-get_byte_select(binary_address,bs_bits,byte_select);
-test_struct(s_cache);
-print_struct(s_cache);
+    hex_to_bin(hex_address,binary_address,HEX);
+    get_byte_select(binary_address,bs_bits,byte_select);
+    test_struct(s_cache);
+    print_struct(s_cache);
 
-printf("Hex: %s\n",hex_address);
-printf("Binary: ");
-for(int i = 0; i < BIN; i++)
-{	
-	printf("%d",binary_address[i]);
-}
-	printf("\n");
-for(int i = 0; i < byte_select; i++)
-{
-	printf("%d",bs_bits[i]);
-}
-	printf("\n");
-return 0;
+    printf("Hex: %s\n",hex_address);
+    printf("Binary: ");
+    for(int i = 0; i < BIN; i++)
+    {	
+        printf("%d",binary_address[i]);
+    }
+        printf("\n");
+    for(int i = 0; i < byte_select; i++)
+    {
+        printf("%d",bs_bits[i]);
+    }
+        printf("\n");
+    return 0;
 
 }
